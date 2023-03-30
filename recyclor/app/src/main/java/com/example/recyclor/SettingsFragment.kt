e34d786389048870721f9b32bd582c1cbba1a3bf
jantsu-rj <jani@rannanjarvi.net>
2023-03-30 16:11:20 +0300

    navigaatio

--- recyclor/app/src/main/java/com/example/recyclor/SettingsFragment.kt
+++ recyclor/app/src/main/java/com/example/recyclor/SettingsFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.commit
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +38,31 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        val btnLogin: Button = view.findViewById<Button>(R.id.buLogin)
+        val btnRegister: Button = view.findViewById<Button>(R.id.buRegister)
+        val themeSwitch: Switch = view.findViewById<Switch>(R.id.switchNightLight)
+        btnLogin.setOnClickListener { replaceFragment(Login()) }
+        btnRegister.setOnClickListener { replaceFragment(SignUp()) }
+        themeSwitch.setOnCheckedChangeListener { _, b ->
+            when (b) {
+                true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+                false -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+        val isNightModeOn: Boolean = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+        println(isNightModeOn)
+        themeSwitch.isChecked = isNightModeOn
+
+        return view
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = parentFragmentManager
+        fragmentManager.commit {
+            replace(R.id.frame_layout, fragment)
+        }
     }
 
     companion object {
