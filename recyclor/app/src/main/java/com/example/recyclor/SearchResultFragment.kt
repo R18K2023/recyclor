31d99f11f8d4cdd9dcfaf0a0579abbbf498a801d
t1sasa03 <t1sasa03@students.oamk.fi>
2023-04-16 23:32:29 +0300

    lisätty firebase hakutoimintoon

--- recyclor/app/src/main/java/com/example/recyclor/SearchResultFragment.kt
+++ recyclor/app/src/main/java/com/example/recyclor/SearchResultFragment.kt
@@ -1,11 +1,12 @@
 package com.example.recyclor
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 
 
 class SearchResultFragment : Fragment() {
@@ -23,16 +24,25 @@ class SearchResultFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val wasteNameTV=view.findViewById<TextView>(R.id.tv_jätteenNimi)
-        val wasteDetailTV=view.findViewById<TextView>(R.id.tv_lajitteluohje)
-        val wasteWhereToTV=view.findViewById<TextView>(R.id.tv_minneOhjeet)
+        val wasteNameTV = view.findViewById<TextView>(R.id.tv_jätteenNimi)
+        val wasteDetailTV = view.findViewById<TextView>(R.id.tv_lajitteluohje)
+        val wasteWhereToTV = view.findViewById<TextView>(R.id.tv_minneOhjeet)
+        val showMap = view.findViewById<Button>(R.id.btn_keräyspisteet)
 
-        val wasteName =arguments?.getString("title")
+        val wasteName = arguments?.getString("title")
         val wasteDetail = arguments?.getString("detail")
-        val wasteWhereTo=arguments?.getString("where")
+        val wasteWhereTo = arguments?.getString("where")
 
-        wasteNameTV.text=wasteName
-        wasteDetailTV.text=wasteDetail
-        wasteWhereToTV.text=wasteWhereTo
+        wasteNameTV.text = wasteName
+        wasteDetailTV.text = wasteDetail
+        wasteWhereToTV.text = wasteWhereTo
+
+        showMap.setOnClickListener {
+            val targetFragment = MapsFragment()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, targetFragment)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
