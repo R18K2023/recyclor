abc7e12e166e29ee761931b66721d7c168c1105d
jantsu-rj <jani@rannanjarvi.net>
2023-04-18 12:52:27 +0300

    Oulun kierrätyspisteet kartalle

--- recyclor/app/src/main/java/com/example/recyclor/MapsFragment.kt
+++ recyclor/app/src/main/java/com/example/recyclor/MapsFragment.kt
@@ -25,24 +25,38 @@ import com.example.recyclor.model.RecyclingPointsResponse
 
 class MapsFragment : Fragment() {
     private val coroutineScope = CoroutineScope(Dispatchers.Default)
-    private suspend fun getRecyclingPoints() {
+    private suspend fun getRecyclingPoints(): RecyclingPointsResponse {
         val client = HttpClient(CIO) {
             install(ContentNegotiation) {
                 gson()
             }
         }
         val url = URL("http://api.kierratys.info/collectionspots/?api_key=83926c47ccaf5260ee9d2e597923190377aa7200&limit=265&municipality=Oulu")
-        val recyclingPoints: RecyclingPointsResponse = client.get(url).body()
 
-        println(recyclingPoints)
+        return client.get(url).body()
     }
 
 
     private val callback = OnMapReadyCallback { googleMap ->
+       coroutineScope.launch(Dispatchers.Main) {
+            val recyclingPoints =  getRecyclingPoints()
+           for (item in recyclingPoints.results.orEmpty()) {
+               if (item != null) {
+                   val pointLat = item.geometry?.coordinates?.get(1)
+                   val pointLng = item.geometry?.coordinates?.get(0)
+                   val pointName = item.name
+                   if (pointLat != null) {
+                       val pointCoordinates = LatLng(pointLat as Double, pointLng as Double)
+                       googleMap.addMarker(
+                           MarkerOptions().position(pointCoordinates).title(pointName)
+                       )
+                   }
+               }
+           }
+           val oulu = LatLng(65.01, 25.46)
+           googleMap.moveCamera(CameraUpdateFactory.newLatLng(oulu))
+        }
 
-        val oulu = LatLng(65.01, 25.46)
-        googleMap.addMarker(MarkerOptions().position(oulu).title("Marker in Oulu"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(oulu))
     }
 
     override fun onCreateView(
@@ -56,7 +70,6 @@ class MapsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        coroutineScope.launch { getRecyclingPoints() }
         mapFragment?.getMapAsync(callback)
     }
 }
